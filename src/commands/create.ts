a5487c896689096719bb2dd441f31ee576cbe3e2
Syakir Rahman <syakirurahman@gmail.com>
2024-08-19 11:22:26 +0700

    feat: add spinner

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,5 +1,6 @@
 import { Command } from 'commander';
 import prompts from 'prompts';
+import ora from 'ora';
 
 export const create = new Command() .name("create")
   .description("Create a file with JSON content")
@@ -16,4 +17,20 @@ export const create = new Command() .name("create")
       }
     ]);
     console.log("Create file:", options.create);
+    if (options.create) {
+      // Create and start a spinner
+      const spinner = ora('Starting the file creation process...').start();
+      
+      // Simulate a task
+      setTimeout(() => {
+          spinner.succeed('File creation completed!');
+          // You can replace the setTimeout with actual logic
+      }, 2000); // Simulate a 2-second task
+    }
+
+    // Run with npm:
+    // npm run build
+    // npm run start create --fileName test
+    // choose 'yes' for prompt
+    // the spinner will show
   });
