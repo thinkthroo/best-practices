4923aa3dc07c9ec2a971aacf5af1f28bb7d4f7f5
Syakir Rahman <syakirurahman@gmail.com>
2024-08-19 10:37:49 +0700

    feat: initialized commanderjs and created example prompt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export default function index() {
-  console.log("Hello world");
-}
+import { Command } from 'commander';
+
+const program = new Command(); program .option('--first') .option('-s, --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+
+console.log(program.args[0].split(options.separator, limit));
+
+// Run with npm:
+// npm run build
+// npm run start -- -s / --first a/b/c
+
+// Run with pnpm:
+// pnpm run build
+// pnpm run start -s / --first a/b/c
