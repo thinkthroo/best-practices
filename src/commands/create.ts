582d4480ef46620d9e35e8bb8433b9074ff03063
Syakir Rahman <syakirurahman@gmail.com>
2024-08-19 10:54:15 +0700

    feat: add prompt 'Would you like to create a file with JSON content?'

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,9 +1,17 @@
-import { Command } from 'commander'; 
+import { Command } from 'commander'; import prompts from 'prompts';
 
-export const create = new Command() 
-  .name("create") 
-  .description("Create a new resource") 
-  .argument("<fileName>", "the name of the file to create")    
-  .action((fileName) => { 
-    console.log(`Creating a new file named ${fileName}`)
-});
+export const create = new Command() .name("create")
+  .description("Create a file with JSON content")
+  .argument("fileName", "The file name to create")
+  .action(async (fileName: string, opts: any) => {
+    const options = await prompts([
+      { type: "toggle",
+        name: "create",
+        message: 'Would you like to create a file with JSON content?',
+        initial: true,
+        active: "yes",
+        inactive: "no",
+      }
+    ]);
+    console.log("Create file:", options.create);
+  });
